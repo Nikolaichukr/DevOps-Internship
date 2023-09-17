4582cb28bfc6817eef8fd60b7d44447a7504bb9a
Roman Nikolaichuk <51542378+Nikolaichukr@users.noreply.github.com>
2023-09-17 22:37:03 +0300

    Terraform Task 2 Solution

--- 6. Terraform/Task2/modules/ec2/outputs.tf
+++ 6. Terraform/Task2/modules/ec2/outputs.tf
@@ -1,10 +1,10 @@
 output "ec2_ami" {
-  value = ""
+  value = aws_instance.my_Amazon_Linux[*].ami
 }
 output "ec2_public_ip" {
-  value = ""
+  value = aws_instance.my_Amazon_Linux[*].public_ip
 }
 
 output "ec2_type" {
-  value = ""
+  value = aws_instance.my_Amazon_Linux[*].instance_type
 }
